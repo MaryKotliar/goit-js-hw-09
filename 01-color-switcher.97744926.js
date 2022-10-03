5762eb7a8a5bc3e8445313414f5feb4183d8cd30
MaryKotliar <105055199+MaryKotliar@users.noreply.github.com>
2022-10-03 13:06:07 +0000

    Deploying to gh-pages from @ MaryKotliar/goit-js-hw-09@b4bb727d29bb2d8774820249d09ec3efacfa8338 🚀

---
+++ 01-color-switcher.97744926.js
@@ -0,0 +1,2 @@
+const t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]"),r=document.querySelector("body");let n=null;t.addEventListener("click",(function(t){n=setInterval((()=>{r.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16)}`}),1e3),t.currentTarget.disabled=!0,e.disabled=!1})),e.addEventListener("click",(function(e){clearInterval(n),e.currentTarget.disabled=!0,t.disabled=!1}));
+//# sourceMappingURL=01-color-switcher.97744926.js.map
---
+++ 01-color-switcher.db941a82.js
@@ -0,0 +1,2 @@
+!function(){var t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]"),n=document.querySelector("body"),r=null;t.addEventListener("click",(function(t){r=setInterval((function(){n.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16))}),1e3),t.currentTarget.disabled=!0,e.disabled=!1})),e.addEventListener("click",(function(e){clearInterval(r),e.currentTarget.disabled=!0,t.disabled=!1}))}();
+//# sourceMappingURL=01-color-switcher.db941a82.js.map
